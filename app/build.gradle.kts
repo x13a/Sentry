8fdb9cb0ee12ab39090b0b21c7bc4028291961f9
lucky <>
2026-09-22 18:51:28 +0300

    migrate to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "me.lucky.sentry"
+    compileSdk {
+        version = release(37)
+    }
+
+    defaultConfig {
+        applicationId = "me.lucky.sentry"
+        minSdk = 23
+        targetSdk = 32
+        versionCode = 10
+        versionName = "1.1.3"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    lint {
+        disable.add("MissingTranslation")
+    }
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.material)
+    implementation(libs.androidx.preference.ktx)
+    implementation(libs.androidx.biometric)
+    implementation(libs.androidx.drawerlayout)
+    implementation(libs.guardianproject.panic)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+}
